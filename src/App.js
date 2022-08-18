50e89298344a3aa4ed3736112520bb064f28069a
owusuvictorjnr <owusuvictor80@gmail.com>
2022-08-19 00:59:19 +0100

    Footer and image commented out

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+// import Images from "./components/Images";
+// import Footer from "./components/Footer"
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      {/* <Footer /> */}
+      {/* <Images/> */}
+     
     </div>
   );
 }
---
+++ src/components/Images.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+// import pic1 from "../assets/images/pic1.jpg";
+
+// export const Images = () => {
+//   return (
+//     <div className="h-60">
+//       <img src={pic1} alt="me" />
+//     </div>
+//   );
+// };
+
+// export default Images;
