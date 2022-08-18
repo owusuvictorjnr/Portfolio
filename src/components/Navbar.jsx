ae049ca1f657cd272a604c3e58483ff1bc3a04db
owusuvictorjnr <owusuvictor80@gmail.com>
2022-08-19 00:45:13 +0100

    Navbar of the portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,15 @@ import NavItems from "./NavItems";
 
 function Navbar(){
     return(
-        <nav className="bg-black mt-20 text-green-100 flex justify-between py-5 px-20 shadow-md">
-            <h3 className="font-bold">
+        <nav className="fixed top-0 right-0 left-0  bg-black  text-green-100 flex justify-between py-5 px-20 shadow-md">
+            <h3 className="font-bold text-red-400">
                 Vitech Solutions
             </h3>
             <ul className="flex gap-20">
-                <NavItems menu={<a href="Home">Home</a>}/>
+                <NavItems menu={<a href="Home">Home</a> }/>
                 <NavItems menu={<a href="about">About</a>}/>
                 <NavItems menu={<a href="contact">Contact</a> }/>
                 <NavItems menu={<a href="resume">Resume</a> }/>
-                {/* <NavItems menu={""}/>
-                <NavItems menu={"Home"}/> */}
             </ul>
         </nav>
     )
